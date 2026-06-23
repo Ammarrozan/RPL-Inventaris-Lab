d091722304e20a93399ed575d0c52e001430f819
yardann09 <yardanlaksana@gmail.com>
2026-06-23 07:06:25 +0700

    new

--- peminjaman-lab/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ peminjaman-lab/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,17 +11,13 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
+
     public function create(): View
     {
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
+
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
@@ -39,9 +35,7 @@ class AuthenticatedSessionController extends Controller
         return redirect('/');
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
+    
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
